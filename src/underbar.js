615d194d039fb901b4b4321b6692d28d5162943c
Simone <your_email@whatever.com>
2016-06-23 18:13:48 +0900

    Stuck at _.contains

--- src/underbar.js
+++ src/underbar.js
@@ -174,7 +174,6 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
-    var total = 0;
     var previousCall = 0;
     if (accumulator == undefined) {
       var newAccumulator = collection[0];
@@ -197,13 +196,20 @@
   _.contains = function(collection, target) {
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
-    return _.reduce(collection, function(wasFound, item) {
-      if (wasFound) {
-        return true;
+
+      if (Array.isArray(collection)) {
+        return _.reduce(collection, function(wasFound, item) {
+          if (wasFound) {
+            return true;
+          }
+          return item === target;
+        }, false);
       }
-      return item === target;
-    }, false);
-  };
+// stuck here. not sure how to make it different so that it works for objects too
+      else {
+      
+      }
+};
 
 
   // Determine whether all of the elements match a truth test.
